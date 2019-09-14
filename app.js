59f9335c35b8645a05b9d201042fec899de2634a
Ronny Visa <ronny.visa@gmail.com>
2019-09-13 23:42:58 -0500

    new version for API express

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { hello: 'This is the response of the GET' };
-    res.json(response);
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find( query, (err, books) => {
+        return (err) ? res.send(err) : res.json(books);
+      }
+    );
+  });
+
+  bookRouter.route('/books/:bookId')
+  .get((req, res) => {    
+    Book.findById( req.params.bookId, (err, book) => {
+        return (err) ? res.send(err) : res.json(book);
+      }
+    );
   });
 
 app.use('/api', bookRouter);
