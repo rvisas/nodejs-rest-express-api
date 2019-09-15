e48943baf76867e100e6bd1ee573b0838345a98f
Ronny Visa <ronny.visa@gmail.com>
2019-09-14 21:19:39 -0500

    new method for the rest api

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-bookRouter.route('/books')
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => 
-      (err) ? res.send(err) : res.json(books)
-    );
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) =>
-      (err) ? res.send(err) : res.json(book)
-    );
-  });
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 
